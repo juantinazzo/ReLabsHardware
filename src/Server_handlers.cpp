914f743a37d69cc9708b6585c1a0208f1f45e473
juantinazzo <juantinazzo@gmail.com>
2022-02-18 18:17:14 -0300

    Handlers actualizados para nuevas librerías de red

--- src/Server_handlers.cpp
+++ src/Server_handlers.cpp
@@ -5,6 +5,9 @@
 #include "IO_expander.h"
 #include "Voltage_Outputs.h"
 #include <aWOT.h>
+#include <ArduinoJson.h>
+
+DynamicJsonDocument doc(256);
 
 void indexCmd(Request &req, Response &res)
 {
@@ -21,5 +24,118 @@ void indexCmd(Request &req, Response &res)
         "</html>";
 
     res.set("Content-Type", "text/html");
+    res.set("Access-Control-Allow-Origin", "*");
     res.printP(index);
+}
+
+void handleAnalogInputs(Request &req, Response &res)
+{
+
+    String temp;
+    char argNameC[10], valueC[2];
+    String message = "{\"data\":[\n";
+    while (req.left())
+    {
+        if (req.form(argNameC, 10, valueC, 10))
+        {
+            String argName = String(argNameC);
+            String value = String(valueC);
+            if (argName == "ADCREAD")
+            {
+                measureAndReturn(value.toInt(), &temp);
+                message += "\t{ " + temp + " }";
+                message += ",\n";
+            }
+            if (argName == "ADCRAW")
+            {
+                measureAndReturnRAW(value.toInt(), &temp);
+                message += "\t{ " + temp + " }";
+                message += ",\n";
+            }
+        }
+        else
+        {
+            return res.sendStatus(400);
+        }
+    }
+    message = message.substring(0, message.length() - 2);
+    message += "\n]}";
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Content-Type", "text/plain");
+    res.print(message);
+    res.status(200);
+}
+
+void handleAnalogOutputs(Request &req, Response &res)
+{
+
+    String message = "{\"data\":[\n";
+
+    String temp;
+    char argNameC[10], valueC[2];
+    while (req.left())
+    {
+        if (req.form(argNameC, 10, valueC, 10))
+        {
+            String argName = String(argNameC);
+            String value = String(valueC);
+            if (argName.substring(0, 4) == "VSET")
+            {
+                setChannelVoltage((char)argName.substring(4).toInt(), value.toFloat());
+            }
+        }
+        else
+        {
+            return res.sendStatus(400);
+        }
+    }
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Content-Type", "text/plain");
+    res.print("{\"status\":\"ok\"}");
+    res.status(200);
+}
+
+void handleConfigGains(Request &req, Response &res)
+{
+
+    String temp;
+    char argNameC[10], valueC[2];
+    String message = "{\"data\":[\n";
+    while (req.left())
+    {
+        if (req.form(argNameC, 10, valueC, 10))
+        {
+            String argName = String(argNameC);
+            String value = String(valueC);
+            deserializeJson(doc, argName);
+            for (uint8_t j = 0; j < 6; j++)
+            {
+                LoadAnalogInputGains(argName.toInt(), j, doc[String(j)]);
+            }
+        }
+    }
+
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Content-Type", "text/plain");
+    res.print("{\"status\":\"ok\"}");
+    res.status(200);
+}
+
+void handleReadGains(Request &req, Response &res)
+{
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Content-Type", "text/plain");
+    res.print(ReturnAnalogInputGains());
+    res.status(200);
+}
+
+void handleStatus(Request &req, Response &res)
+{
+    String status = "[{\"data\":";
+    status += printStatusADS();
+    status += ", " + printStatusExpander();
+    status += "]}";
+    res.set("Access-Control-Allow-Origin", "*");
+    res.print(status);
+    res.status(200);
 }
