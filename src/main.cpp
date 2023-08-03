82938d3fed4baf5ee48cc0af1b85042fc371361e
Juan Tinazzo <juantinazzo@gmail.com>
2023-08-03 11:05:43 -0300

    New MPU6050 LIB

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,18 @@
 #include "systemManager.h"
 #include <ArduinoOTA.h>
 #include <Preferences.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
 #include "network/Passwords.h"
 #include "state_machines.h"
+#include "Simple_MPU6050.h"
+
+#define OFFSETS  1474,   232,   1004,   4294967221,   41,   8
+
+//Adafruit_MPU6050 mpu;
 
 
-
-Adafruit_MPU6050 mpu;
+#define MPU6050_DEFAULT_ADDRESS     0x68    // address pin low (GND), default for InvenSense evaluation board
+Simple_MPU6050 mpu2;
+const char DEVICE_NAME[] = "mpu6050";
 
 unsigned long _time[2000][2]={}, _start_time=0;
 float _angle[2000][2]={};
@@ -59,127 +62,80 @@ EthernetModule EM;
 
 bool expanderStatus[8];
 
-bool initMPU(){
-     Serial.println("\nAdafruit MPU6050 test!");
-
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    return false;
-  }
-  Serial.println("MPU6050 Found!");
-
-  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-  mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-  return true;
-}
-
 #define A_R 16384.0 // 32768/2
 #define G_R 131.0 // 32768/250
  
 //Conversion de radianes a grados 180/PI
 #define RAD_A_DEG = 57.295779
 float Acc[2];
+float roll;
+float pitch;
+float combinedAngle;
 float Gy[3];
 float Angle[3];
 long tiempo_prev;
 float dt;
 String valores;
 
-void updateMPU(){
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+#define FILTER_CONSTANT 0.8
 
-  Acc[1] = atan(-1*(a.acceleration.x/A_R)/sqrt(pow((a.acceleration.y/A_R),2) + pow((a.acceleration.z/A_R),2)))*RAD_TO_DEG;
-  Acc[0] = atan((a.acceleration.y/A_R)/sqrt(pow((a.acceleration.x/A_R),2) + pow((a.acceleration.z/A_R),2)))*RAD_TO_DEG;
-  Gy[0] = g.gyro.x/G_R;
-  Gy[1] = g.gyro.y/G_R;
-  Gy[2] = g.gyro.z/G_R;
+// See mpu.on_FIFO(print_Values); in the Setup Loop
+void print_Values (int16_t *gyro, int16_t *accel, int32_t *quat) {
+  Quaternion q;
+  VectorFloat gravity;
+  float ypr[3] = { 0, 0, 0 };
+  float xyz[3] = { 0, 0, 0 };
+  mpu2.GetQuaternion(&q, quat);
+  mpu2.GetGravity(&gravity, &q);
+  mpu2.GetYawPitchRoll(ypr, &q, &gravity);
+  mpu2.ConvertToDegrees(ypr, xyz);
 
-   dt = (millis() - tiempo_prev) / 1000.0;
-   tiempo_prev = millis();
- 
-   //Aplicar el Filtro Complementario
-   Angle[0] = 0.95 *(Angle[0]+Gy[0]*dt) + 0.05*Acc[0];
-   Angle[1] = 0.95 *(Angle[1]+Gy[1]*dt) + 0.05*Acc[1];
+  combinedAngle=acos(cos(xyz[2]/RAD_TO_DEG)*cos(xyz[1]/RAD_TO_DEG))*RAD_TO_DEG;
 
-   //Integración respecto del tiempo paras calcular el YAW
-   Angle[2] = Angle[2]+Gy[2]*dt;
-
-   valores = String(Angle[0]) + "," + String(Angle[1]) + "," + String(Angle[2]);
-   /**Serial.print(valores);
-  Serial.print("  Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");*/
   index_array_pos++;
   if(index_array_pos>=2000){
     index_array_pos=0;
     index_use_array=index_use_array==0?1:0;
   }
   _time[index_array_pos][index_use_array]=millis()-_start_time;
-  _angle[index_array_pos][index_use_array]=Angle[0];
+  _angle[index_array_pos][index_use_array]=combinedAngle;//Angle[0];
+ /* Serial.print(xyz[0]);
+  Serial.print(", ");
+  Serial.print(xyz[1]);
+  Serial.print(", ");
+  Serial.println(xyz[2]);*/
+   //Serial.printfloatx(F("Yaw")  , xyz[0],   9, 4, F(",   ")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    //Serial.printfloatx(F("Pitch"), xyz[1],   9, 4, F(",   "));
+    //Serial.printfloatx(F("Roll") , xyz[2],   9, 4, F("\n"));
+   /* Serial.printfloatx(F("ax")   , accel[0], 5, 0, F(",   "));
+    Serial.printfloatx(F("ay")   , accel[1], 5, 0, F(",   "));
+    Serial.printfloatx(F("az")   , accel[2], 5, 0, F(",   "));
+    Serial.printfloatx(F("gx")   , gyro[0],  5, 0, F(",   "));
+    Serial.printfloatx(F("gy")   , gyro[1],  5, 0, F(",   "));
+    Serial.printfloatx(F("gz")   , gyro[2],  5, 0, F("\n"));*/
 }
 
+void mpu_setup()
+{
+  
+  //mpu2.begin();
+  mpu2.begin(SDA, SCL);
+  // Setup the MPU
+  mpu2.Set_DMP_Output_Rate_Hz(200);           // Set the DMP output rate from 200Hz to 5 Minutes.
+  //mpu.Set_DMP_Output_Rate_Seconds(10);   // Set the DMP output rate in Seconds
+  //mpu.Set_DMP_Output_Rate_Minutes(5);    // Set the DMP output rate in Minutes
+  mpu2.SetAddress(MPU6050_DEFAULT_ADDRESS); //Sets the address of the MPU.
+  //mpu2.CalibrateMPU();                      // Calibrates the MPU.
+  mpu2.load_DMP_Image(OFFSETS);                    // Loads the DMP image into the MPU and finish configuration.
+  mpu2.on_FIFO(print_Values);               // Set callback function that is triggered when FIFO Data is retrieved
+  // Note that these funcitons return pointers to themselves so you can write them in one line
+  // mpu.Set_DMP_Output_Rate_Hz(4).SetAddress(MPU6050_DEFAULT_ADDRESS).CalibrateMPU().load_DMP_Image().on_FIFO(print_Values);
+  // Setup is complete!
+}
 
 void mpuTask(void *){
     while(true){
-        updateMPU();
+        //updateMPU();
         delay(5);
     }
     vTaskDelete(NULL);
@@ -200,7 +156,12 @@ void setGetsPosts()
 
 void test_motor_task(void *pvParam){
   while(true){
-
+  Serial.print(Angle[0]);
+  Serial.print(",");
+  Serial.print(Angle[1]);
+  Serial.print(",");
+  Serial.println(combinedAngle);
+  delay(10);
   }
   vTaskDelete(NULL);
 }
@@ -227,6 +188,7 @@ void setup()
     //ArduinoOTA.begin();
     LOG("WiFi connected", Info, sys);
     LOG("IP address: " + WiFi.localIP().toString(), Info, sys);
+    Serial.println("Hola");
 #endif
 
     setGetsPosts();
@@ -249,14 +211,20 @@ void setup()
     //sM.startIO(0);
     //sM.startSERVO(SPARE_IO0);
     //sM.startEXP(0);
-    if(initMPU()) xTaskCreatePinnedToCore(mpuTask,"mpu",2048,NULL,3,NULL,1);
-        xTaskCreatePinnedToCore(pendulo_task,"pendulo_task", 8192,NULL,2,NULL,1);
+    Serial.println("Hola2");
+    mpu_setup();
+    xTaskCreatePinnedToCore(pendulo_task,"pendulo_task", 8192,NULL,2,NULL,1);
+    Serial.println("Hola3");
+    //xTaskCreatePinnedToCore(test_motor_task,"test", 2048,NULL,1,NULL,1);
     
+    Serial.println("Hola4");
  
 }
 
 void loop()
 {
+  mpu2.dmp_read_fifo(false);
+  delay(1);
 
 #ifdef USE_WIFI
     //ArduinoOTA.handle();
